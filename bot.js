c668bc2a8e899e93039883568ce9632786011909
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-06-22 05:14:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-      if(!message.member.hasPermission("SEND MESSAGES")) return;
+      if(!message.member.hasPermission("ADMINISTRATOR")) return;
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
