44556fa3bd5db5011d22a9ff8d8530b6c62901bd
AgentHi5 <booperdooperwin7@gmail.com>
2018-06-22 10:05:53 -0400

    Changes

--- bot.js
+++ bot.js
@@ -79,6 +79,42 @@ bot.on("message", async message => {
         }
 
     }
+
+    if(command === `${prefix}setstatus`) {
+        if(message.author.id === ownerID) {
+            // Sets the bot's status to idle
+            if(args == 'idle') {
+                bot.user.setStatus('idle');
+                message.channel.send(`Status set to idle.`);};
+
+            if(args == 'do not disturb') {
+                bot.user.setStatus('dnd')
+                message.channel.send(`Status set to ${args}`)
+            }
+
+            // Sets the bot's status to dnd
+            if(args == 'dnd') {
+                bot.user.setStatus('dnd');
+                message.channel.send('Status set to dnd.');
+            };
+
+            // Sets the bot's status to invisible
+            if(args == 'invisible') {
+                bot.user.setStatus('invisible');
+                message.channel.send('Status set to invisible');
+            };
+            // Sets the bot's status to online
+            if(args == 'online') {
+                bot.user.setStatus('online');
+                message.channel.send('Status set to online');
+            };
+            // Set's bot's status to default status (aka online)
+            if(args == '') {
+                bot.user.setStatus('online');
+                message.channel.send('Default status set');
+            };
+        }    
+    }
        
 
 });
