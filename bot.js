2bddb264c7c6e68ecf74e4d3623923c2150118d4
AgentHi5 <booperdooperwin7@gmail.com>
2018-06-28 18:14:01 -0400

    setname command

--- bot.js
+++ bot.js
@@ -170,7 +170,7 @@ bot.on("message", async message => {
         message.channel.send(gamestatusembed);
     }
 
-    // e.help - for those people who don't have the code in front of them
+    // help - for those people who don't have the code in front of them
     if(command === `${prefix}help`) {
         let helpembed = new Discord.RichEmbed
             .setAuthor(`Help`)
@@ -178,6 +178,12 @@ bot.on("message", async message => {
             .addField(`Commands:`, `help - You know about this command.\nrestart - Only the owner of this bot can use this command.\nkick - You will need the "KICK_MEMBERS" permission to kick users.\nFUCK - It's pretty obvious what this command is.\nsetstatus - Sets the bot's status.\nsetgame - Sets the bot's game.`)
             .setFooter(`${message.createdAt} Don't delete this because it was a pain to make -_- `);
         message.channel.send(helpembed);
+    }
+
+    // setname - change the bot's name
+    if(command === `${prefix}setname`) {
+        bot.user.setUsername(args);
+        message.channel.send(`I changed my username to \`${args}\``);
     }
 
 
