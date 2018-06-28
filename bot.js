fe6aeff8107c65cb8ca2be3dc0581ba8b4d4ae82
AgentHi5 <booperdooperwin7@gmail.com>
2018-06-28 18:50:57 -0400

    Deleted name command cause that boi broken

--- bot.js
+++ bot.js
@@ -181,11 +181,7 @@ bot.on("message", async message => {
         message.channel.send(helpembed);
     }
 
-    // setname - change the bot's name
-    if(command === `${prefix}setname`) {
-        bot.user.setUsername(argresult);
-        message.channel.send(`I changed my username to \`${argresult}\``);
-    }
+    
 
 
         
