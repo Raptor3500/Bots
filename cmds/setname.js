e3edc967677119bb9b10cc77732d8d47c4a2a51b
AgentHi5 <booperdooperwin7@gmail.com>
2018-06-29 12:38:03 -0400

    Removed all cmds for now

--- cmds/setname.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-  // setname - change the bot's name
-  bot.user.setUsername(argresult);
-  message.channel.send(`I changed my username to \`${argresult}\``);
-}
-
-module.exports.help = {
-  name: "setname"
-}
