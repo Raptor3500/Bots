4e313820045134173780800dc1ba3dad19d43c48
AgentHi5 <booperdooperwin7@gmail.com>
2018-06-29 00:04:59 -0400

    Adding commands to the "cmds" folder :)

--- bot.js
+++ bot.js
@@ -202,16 +202,7 @@ bot.on("message", async message => {
         message.channel.send(`I changed my username to \`${argresult}\``);
     }
 
-    //play
-    function play(connection, message) {
-        var server = servers[message.guild.id];
-        server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
-        server.queue.shift();
-        server.dispatcher.on("end", function() {
-            if(server.queue[0]) play(connection, message);
-            else connection.disconnect();
-        })
-    }
+
     var servers = {};
     module.exports.run = (bot, message, args) => {
         //play
---
+++ cmds/stop.js
@@ -0,0 +1,16 @@
+//stop
+const Discord = require("discord.js");
+const YTDL = require("ytdl-core");
+function play(connection, message) {
+    var server = servers[message.guild.id];
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+    server.queue.shift();
+    server.dispatcher.on("end", function() {
+        if(server.queue[0]) play(connection, message);
+        else connection.disconnect();
+    })
+}
+var servers = {};
+module.exports.help = {
+    name: "stop"
+}
