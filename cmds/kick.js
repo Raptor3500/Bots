f8a0719ed1e2b74073bba5ee1ac5a9f3d9792ed3
AgentHi5 <booperdooperwin7@gmail.com>
2018-06-29 08:09:08 -0400

    kick

--- cmds/kick.js
+++ cmds/kick.js
@@ -4,13 +4,13 @@ module.exports.run = async (bot, message, args) => {
   //!kick @unrealism Because fuck you
 
   let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0
-    if(!kUser) return message.channel.send("Can't find user!");
-    let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("no");
-    if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("That person can't be kicked!");
-    if(kReason == '') {
-        kReason = 'No reason was stated.'
-    }
+  if(!kUser) return message.channel.send("Can't find user!");
+  let kReason = args.join(" ").slice(22);
+  if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("no");
+  if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("That person can't be kicked!");
+  if(kReason == '') {
+    kReason = 'No reason was stated.'
+  }
 
   let kickEmbed = new Discord.RichEmbed()
     .setDescription("~Kick~")
